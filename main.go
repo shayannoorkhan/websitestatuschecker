a0282039b882ed11f38ece7d5cc48bf931dbd11f
shayannoorkhan <shayannoor123khan@yahoo.com>
2022-10-09 19:58:13 -0800

    Adding code for channels

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -12,16 +13,28 @@ func main() {
 		"http://golang.org",
 	}
 
+	c := make(chan string)
+
 	for _, website := range websites {
-		checkwebsite(website)
+		go checkwebsite(website, c)
 	}
+
+	for l := range c {
+		go func(website string) {
+			time.Sleep(5 * time.Second)
+			checkwebsite(website, c)
+		}(l)
+	}
+
 }
 
-func checkwebsite(website string) {
+func checkwebsite(website string, c chan string) {
 	_, err := http.Get(website)
 	if err != nil {
 		fmt.Println(website, "might be down!")
+		c <- website
 		return
 	}
 	fmt.Println(website, "is up!")
+	c <- website
 }
